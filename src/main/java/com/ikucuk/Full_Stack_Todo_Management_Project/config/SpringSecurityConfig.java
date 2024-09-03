15ab532133a361cfdde488c11742ce39d1cd087b
ikucuk <incikucuk98@gmail.com>
2024-09-03 12:04:14 +0300

    Created role-based authentication-authorization wit method-level security in SecurityFilterChain class

---
+++ src/main/java/com/ikucuk/Full_Stack_Todo_Management_Project/config/SpringSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.ikucuk.Full_Stack_Todo_Management_Project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    DefaultSecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //method level security
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> {
+           authorize.requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN");
+            authorize.requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("ADMIN","USER");
+            authorize.requestMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("ADMIN","USER");
+            //authorize.requestMatchers(HttpMethod.GET, "/api/**").permitAll();   //tum kullanicilara acik
+
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails inciUser = User.builder()
+                .username("inci")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(inciUser,admin);
+    }
+}
